90161fbea4abc45ff9b2470c5c975a561aba30de
gaeul <e0540@hanmail.net>
2022-11-01 10:46:03 +0900

    페이지네이션 완료

--- index.php
+++ index.php
@@ -1,41 +1,11 @@
 <?php include './dbconnect.php';
-
-// board 테이블에서 pk값을 기준으로 내림차순하여 10개까지 표시
-$sql = "SELECT * FROM board ORDER BY pk desc limit 0,5";
-$result = mysqli_query($conn, $sql);
-
-//총 레코드 수 반환
-$total = mysqli_num_rows($result);
+include './pagenation.php';
 
 //겟요청으로 'page'가 있다면 그 파라미터값, 없으면 1
 $page = isset($_GET['page']) ? $_GET['page'] : 1;
-
-
-function pagenation($total, $page, $post_per_page=10){
-    /* $post_per_page는 한페이지에 보여줄 글의 개수
-    $page_button_num 은 페이지 써진 걸 몇개씩 보여줄지 정하는것  */
-    $page_button_num = 3;
-    // 1page -> 0 / 2page -> 10 / 3page -> 20 \\ 10개씩 끊어 볼 수 있는기준
-    $offset = $post_per_page*($page-1);
-
-    //글을 10개씩 묶었을때 생기는 총페이지수
-    $total_page_num = ceil($total/$post_per_page);
-    // 현재글번호로 글하나를 삭제하더라도 글 번호의 건너뜀없이 번호순서 유지하게 해둠.
-    $current_num = $total - $offset;
-
-    //전체 블록개수
-    $total_block = ceil($total_page_num/$page_button_num);
-    //현재블록
-    $current_block = ceil($page/$page_button_num);
-
-    //페이지 블록이 시작하는 첫 페이지
-    $first = ($current_block-1)*$page_button_num;
-    $last = $current_block * $page_button_num;
-
-    if ($current_block >= $total_block){
-        $last= $total_page_num;
-    }
-}
+$post_per_page= 10;
+list($total_page, $start_page_num, $end_page_num, $offset_result, $total)= pagenation($conn, $page,$post_per_page);
+$cnt = $total-(($page-1)*$post_per_page); 
 ?>
 
 <!DOCTYPE html>
@@ -54,7 +24,7 @@ function pagenation($total, $page, $post_per_page=10){
         <!-- 이건 일단 나중에
             <div class ="search-box">
             </div> -->
-        <div id ="status">Total : <?= $total?> Page : n/n </div>
+        <div id ="status">Total : <?= $total?> Page : <?= $page.'/'.$total_page?> </div>
         <table class="list-table">
             <thead>
                 <tr>
@@ -69,13 +39,11 @@ function pagenation($total, $page, $post_per_page=10){
                 </tr>
             </thead>
 
-            <?php
-            
-            while ($post = mysqli_fetch_assoc($result)) {
-            ?>
+            <?php 
+            while ($post = mysqli_fetch_assoc($offset_result)) {?>
             <tbody>
                 <tr>
-                    <td class="num"><?= $post['pk'];?></td>
+                    <td class="num"><?= $cnt;?></td>
                     <td class="category"><?= $post['category'];?></td>
                     <td class="title"><a href="/board/read.php?index=<?=$post['pk'];?>"><?= $post['title'] ?></a></td>
                     <?php if (strlen($post['userfile'])>1)
@@ -86,8 +54,30 @@ function pagenation($total, $page, $post_per_page=10){
                     <td class="hit"><?= $post['pk'];?></td>
                 </tr>
             </tbody>
-            <?php } mysqli_close($conn); ?>
+            <?php $cnt--;
+            }; ?>
         </table>
+        <p class = "pager">
+            <?php 
+            // 이전페이지
+            if($page > 1) {?>
+                <a href="./index.php?page=1"><<</a>
+                <a href="./index.php?page=<?=($page -1);?>"><</a>
+            <?php } 
+            
+            //페이지 번호 출력
+            for ($print_page = $start_page_num; $print_page <= $end_page_num; $print_page++){?>
+                <a href="./index.php?page=<?= $print_page;?>"><?=$print_page."</a>";}?>
+            
+            <!-- 다음페이지 -->
+            <?php
+            if ($page < $total_page){?>
+                <a href="./index.php?page"<?=($page +1) ?>>></a>
+                <a href="./index.php?page=<?= $total_page;?>">>></a>
+            <?php }?>
+            
+
+
         <div class = "btn">
             <a href="./create_post.php"><button>글쓰기</button></a>
         </div>
--- pagenation.php
+++ pagenation.php
@@ -1,28 +1,49 @@
 <?php
-function pagenation($total, $page, $post_per_page=10){
-    /* $post_per_page는 한페이지에 보여줄 글의 개수
-    $page_button_num 은 페이지 써진 걸 몇개씩 보여줄지 정하는것  */
-    $page_button_num = 3;
-    // 1page -> 0 / 2page -> 10 / 3page -> 20 \\ 10개씩 끊어 볼 수 있는기준
-    $offset = $post_per_page*($page-1);
+include './dbconnect.php';
 
-    //글을 10개씩 묶었을때 생기는 총페이지수
-    $total_page_num = ceil($total/$post_per_page);
-    // 현재글번호로 글하나를 삭제하더라도 글 번호의 건너뜀없이 번호순서 유지하게 해둠.
-    $current_num = $total - $offset;
-
-    //전체 블록개수
-    $total_block = ceil($total_page_num/$page_button_num);
-    //현재블록
-    $current_block = ceil($page/$page_button_num);
-
-    //페이지 블록이 시작하는 첫 페이지
-    $first = ($current_block-1)*$page_button_num;
-    $last = $current_block * $page_button_num;
-
-    if ($current_block >= $total_block){
-        $last= $total_page_num;
-    }
+function pagenation($conn, $page, $post_per_page=10){
+    /* $post_per_page는 한페이지당 데이터 개수
+        $page_num 한 블럭당 페이지 수, $page는 현재 페이지  */
+    $page_num = 3;
     
+    $select_all_sql = "SELECT * FROM board";
+    $select_all_result = mysqli_query($conn, $select_all_sql);
+    $total = mysqli_num_rows($select_all_result);
+
+    //전체 페이지수 = 전체데이터 / 페이지당 데이터 개수, ceil: 올림값, floor: 내림값
+    $total_page = ceil($total/$post_per_page);
+
+    //전체 블록개수 = 전체페이지 수 / 블럭당 페이지 수
+    $total_block = ceil($total_page/$page_num);
+
+    //현재 블럭 번호 = 현재 페이지 번호 / 블럭 당 페이지 수
+    $now_block = ceil($page / $page_num);
+
+    //블럭 당 시작 페이지 번호 = (해당글 블럭번호 -1) * 블럭당 페이지 수 + 1;
+    $start_page_num = ($now_block -1) * $page_num +1;
+
+    //데이터가 0개인경우 페이지는 1이 꼭 있도록
+    if($start_page_num <= 0){
+        $start_page_num = 1;
+    };
+
+    //블럭당 마지막 페이지 번호 = 현재 블럭 번호 * 블럭 당 페이지 수 
+    $end_page_num = $now_block * $page_num;
+
+    //페이지번호의 마지막 번호가 전체 페이지를 넘지 않도록.
+    if($end_page_num > $total_page){
+        $end_page_num = $total_page;
+    };
+
+    //페이지별 게시글 첫번호
+    $start_post_num = ($page -1) * $post_per_page;
+    // 게시글 첫번호 ~ 페이지당 데이터 수로 개수 제한하여 조회
+
+    $offset_sql = "SELECT * FROM board ORDER BY pk desc LIMIT $start_post_num, $post_per_page ";
+    $offset_result = mysqli_query($conn, $offset_sql);
+    mysqli_close($conn);
+
+    return array($total_page, $start_page_num, $end_page_num, $offset_result, $total);
+
 }
 ?>
